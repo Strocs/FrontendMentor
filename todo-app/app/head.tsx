e612ae1d527297a5ba4304ab358e96d5cd45b550
Strocs <iganmolina@gmail.com>
2022-11-04 22:26:24 -0300

    new: adding new challenge

---
+++ todo-app/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head () {
+  return (
+    <>
+      <title>Frontend Mentor | Todo app</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <link rel='icon' href='/favicon-32x32.png' />
+    </>
+  )
+}
